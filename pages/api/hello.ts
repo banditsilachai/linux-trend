d193d02fa40188ade8c2a608bd5c6d4341beda28
Bandit Silachai <mbandit001@yahoo.co.th>
2022-07-20 13:32:41 +0700

    🚨 use cors ext. file

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,36 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import Cors from "cors";
-
-const cors = Cors({
-  methods: ["GET", "HEAD"],
-});
-
-function runMiddleware(
-  req: NextApiRequest,
-  res: NextApiResponse,
-  fn: Function
-) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-
-      return resolve(result);
-    });
-  });
-}
+import middleCors from "../../middleware/cors";
+import { cors } from "../../middleware/cors";
 
 type Data = {
   message: string;
 };
 
 async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  await runMiddleware(req, res, cors);
+  await middleCors(req, res, cors);
 
   // Rest of the API logic
-  res.json({ message: "Hello Everyone!" });
+  res.status(200).json({ message: "Hello Everyone!" });
 }
 
 export default handler;
