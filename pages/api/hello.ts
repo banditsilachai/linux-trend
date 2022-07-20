43904fe0e1867e154f2ceb39f321b2d2e941dd9f
Bandit Silachai <mbandit001@yahoo.co.th>
2022-07-20 13:24:16 +0700

    🚨 test cors

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,36 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import Cors from "cors";
+
+const cors = Cors({
+  methods: ["GET", "HEAD"],
+});
+
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 
 type Data = {
-  name: string
+  message: string;
+};
+
+async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  await runMiddleware(req, res, cors);
+
+  // Rest of the API logic
+  res.json({ message: "Hello Everyone!" });
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+export default handler;
