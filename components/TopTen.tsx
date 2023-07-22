8f5d9ecc75188fc63f88719bb53b4a4e88fc9cd0
Bandit Silachai <mbandit001@yahoo.co.th>
2023-07-22 10:52:19 +0700

    ✨ feature: add TopTen

---
+++ components/TopTen.tsx
@@ -0,0 +1,72 @@
+import type { NextPage } from "next";
+import useSWR from "swr";
+
+import {
+  Title,
+  List,
+  Anchor,
+  Loader,
+  Image,
+  createStyles,
+  Text,
+} from "@mantine/core";
+import getDate from "../data/last1months.json";
+import { formatDistance } from "date-fns";
+
+const useStyles = createStyles((theme, _params) => ({
+  distroName: {
+    filter: "grayscale(50%)",
+    ":hover": {
+      filter: "grayscale(0%)",
+    },
+  },
+}));
+
+const fetcher = (url: any) => fetch(url).then((res) => res.json());
+
+interface DistroProp {
+  no: number;
+  name: string;
+  logo: string;
+  url: string;
+}
+
+const TopTen: NextPage = () => {
+  const url = "/api/last1months?pageSize=10&pageOffset=0";
+  const { data } = useSWR(url, fetcher);
+  const { classes } = useStyles();
+  const updated = formatDistance(new Date(getDate.createAt[0]), new Date(), {
+    addSuffix: true,
+  });
+
+  if (!data) return <Loader />;
+  return (
+    <>
+      <Title>Top 10</Title>
+      <List
+        spacing="xs"
+        center
+        sx={{ paddingTop: 20, paddingBottom: 20, marginLeft: 30 }}
+      >
+        {data?.items.map((item: DistroProp) => {
+          return (
+            <List.Item
+              key={item.no}
+              className={classes.distroName}
+              icon={
+                <Image src={item.logo} width={24} height={24} alt={item.name} />
+              }
+            >
+              <Anchor color="dark" href={item.url}>
+                {item.name}
+              </Anchor>
+            </List.Item>
+          );
+        })}
+      </List>
+      <Text>last update&nbsp;{updated}</Text>
+    </>
+  );
+};
+
+export default TopTen;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,13 @@ import Example from "../components/Example";
 import Resources from "../components/Resources";
 import Layout from "../components/Layout";
 import Hero from "../components/Hero";
+import TopTen from "../components/TopTen";
 
 const Home: NextPage = () => {
   return (
     <Layout>
       <Hero />
+      <TopTen />
       <Example />
       <Resources />
     </Layout>
