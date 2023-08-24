19e2dc39cab957d369b6989f2e4a1a5d1190dab4
Bandit Silachai <mbandit001@yahoo.co.th>
2023-08-24 12:52:15 +0700

    📚 update from script: 24-08-2023 12:52:15

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ current_datetime=$(date +'%d-%m-%Y %H:%M:%S')
 cd /Users/bandit/Development/React/linux-trend/
 
 # Run your Node.js script
-/Users/bandit/Library/Caches/fnm_multishells/95502_1692154086212/bin/node fetch.js
+/Users/bandit/Library/Caches/fnm_multishells/61724_1692856268860/bin/node fetch.js
 
 # Check if there are changes to commit
 if [ -n "$(git status --porcelain)" ]; then
